e68ef185cfcd2791b1666a4330a999d73d07d644
Nate <natediven1@gmail.com>
2022-03-26 23:11:21 -0600

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -46,6 +46,7 @@ mv Assignment06 a-simple-form
 
 echo 'building assignment 7'
 cd Assignment07
+export BASE_PATH=/~nd37549/a-canvas-2d-graphics-scene/ # set the base path for this project
 npm ci 2>> /dev/null
 npm run deploy 2>> /dev/null
 mv dist/ ../a-canvas-2d-graphics-scene # move the build to the project path
